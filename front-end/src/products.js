6b72a0b4eae418a5d861cfa73ba5336ee9424719
Keaton Sentak <k.sentak@outlook.com>
2021-01-20 23:27:37 -0500

    develop products page

--- front-end/src/products.js
+++ front-end/src/products.js
@@ -10,7 +10,7 @@ const products = [
     price: 89.99,
     countInStock: 10,
     rating: 4.5,
-    numReviews: 12,
+    numReviews: 12
   },
   {
     _id: '2',
@@ -23,7 +23,7 @@ const products = [
     price: 599.99,
     countInStock: 7,
     rating: 4.0,
-    numReviews: 8,
+    numReviews: 8
   },
   {
     _id: '3',
@@ -36,7 +36,7 @@ const products = [
     price: 929.99,
     countInStock: 5,
     rating: 3,
-    numReviews: 12,
+    numReviews: 12
   },
   {
     _id: '4',
@@ -49,7 +49,7 @@ const products = [
     price: 399.99,
     countInStock: 11,
     rating: 5,
-    numReviews: 12,
+    numReviews: 12
   },
   {
     _id: '5',
@@ -62,7 +62,7 @@ const products = [
     price: 49.99,
     countInStock: 7,
     rating: 3.5,
-    numReviews: 10,
+    numReviews: 10
   },
   {
     _id: '6',
@@ -75,8 +75,8 @@ const products = [
     price: 29.99,
     countInStock: 0,
     rating: 4,
-    numReviews: 12,
-  },
-]
+    numReviews: 12
+  }
+];
 
-export default products
+export default products;
