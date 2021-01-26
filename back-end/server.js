57fb72ac10018152676fc240aa7c36b948cd4878
Keaton Sentak <k.sentak@outlook.com>
2021-01-26 11:36:57 -0500

    user auth endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -10,11 +11,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// Routes
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // Custom error handling
 app.use(notFound);
