767a11ae703ca5c3536c651af9d58aa3f5bbd74d
Keaton Sentak <k.sentak@outlook.com>
2021-01-29 00:04:07 -0500

    order route and controller

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 // Custom error handling
 app.use(notFound);
