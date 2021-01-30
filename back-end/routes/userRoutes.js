4c7a5fe3392bd4d24b3d99993bdc16070a60effa
Keaton Sentak <k.sentak@outlook.com>
2021-01-30 12:58:03 -0500

    updateuser and getuser endpoints

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -5,7 +5,9 @@ import {
   getUserProfile,
   updateUserProfile,
   getUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
@@ -13,6 +15,10 @@ const router = express.Router();
 router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route('/:id').delete(protect, admin, deleteUser);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
