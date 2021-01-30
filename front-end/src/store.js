4477a9f7dcdc45d3404ed6f5b5d16210fb602842
Keaton Sentak <k.sentak@outlook.com>
2021-01-29 21:51:27 -0500

    pay order action and reducer

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -9,7 +9,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer
 } from './reducers/userReducers';
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -20,7 +20,8 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
-  orderDetails: orderDetailsReducer
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
