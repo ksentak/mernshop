aae415fb8d0e67b24969136adda684558c7275a9
Keaton Sentak <k.sentak@outlook.com>
2021-01-31 12:28:47 -0500

    product edit page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,6 +15,7 @@ import OrderPage from './pages/OrderPage';
 import UserListPage from './pages/UserListPage';
 import UserEditPage from './pages/UserEditPage';
 import ProductListPage from './pages/ProductListPage';
+import ProductEditPage from './pages/ProductEditPage';
 
 const App = () => {
   return (
@@ -34,6 +35,7 @@ const App = () => {
           <Route path='/admin/userlist' component={UserListPage} />
           <Route path='/admin/user/:id/edit' component={UserEditPage} />
           <Route path='/admin/productlist' component={ProductListPage} />
+          <Route path='/admin/product/:id/edit' component={ProductEditPage} />
           <Route path='/' exact component={HomePage} />
         </Container>
       </main>
