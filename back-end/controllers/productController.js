8b5f4619478d5388ba8e6f4b341a9ee3449638af
Keaton Sentak <k.sentak@outlook.com>
2021-01-30 20:04:20 -0500

    create,update endpoints

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -38,4 +38,52 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+// @description Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'N/A',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'N/A',
+    category: 'N/A',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'N/A'
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @description Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
