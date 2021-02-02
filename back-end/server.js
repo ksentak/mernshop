42468686a590f260b0a98a83952958866d1c71ce
Keaton Sentak <k.sentak@outlook.com>
2021-02-02 18:40:08 -0500

    heroku deploy

--- back-end/server.js
+++ back-end/server.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -35,6 +31,17 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/front-end/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 // Custom error handling
 app.use(notFound);
